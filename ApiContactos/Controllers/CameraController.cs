f78dfaa6ae333457a758683096927097859b8f52
Jose Ignacio Rodríguez García <JoseIgnacio.Rodriguez@tajamar365.com>
2016-02-02 14:27:57 +0100

    Camara

---
+++ ApiContactos/Controllers/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ContactosModel.Model;
+
+namespace ApiContactos.Controllers
+{
+    public class CameraController : ApiController
+    {
+        [ResponseType(typeof (String))]
+        public IHttpActionResult Post(FotosModel model)
+        {
+            return Ok("");
+        }
+    }
+}
