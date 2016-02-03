39edfc535a4803cc5b3e3bbb56767ceeca9f91f0
Jose Ignacio Rodríguez García <JoseIgnacio.Rodriguez@tajamar365.com>
2016-02-03 10:23:58 +0100

    Cambios para la clase foto

--- ApiContactos/Controllers/CameraController.cs
+++ ApiContactos/Controllers/CameraController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
+using ApiContactos.Utils;
 using ContactosModel.Model;
 
 namespace ApiContactos.Controllers
@@ -14,7 +16,15 @@ namespace ApiContactos.Controllers
         [ResponseType(typeof (String))]
         public IHttpActionResult Post(FotosModel model)
         {
-            return Ok("");
+            var cuenta = ConfigurationManager.AppSettings["cuenta"];
+            var clave = ConfigurationManager.AppSettings["clave"];
+            var contenedor = ConfigurationManager.AppSettings["contenedor"];
+
+            var sto=new AzureStorageUtils(cuenta,clave,contenedor);
+            var nombre = Guid.NewGuid() + ".jpg";
+            sto.SubirFichero(Convert.FromBase64String(model.Data),nombre);
+
+            return Ok(nombre);
         }
     }
 }
---
+++ ApiContactos/Utils/AzureStorageUtils.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace ApiContactos.Utils
+{
+    public class AzureStorageUtils
+    {
+            private CloudStorageAccount _cuenta; //objeto que almacena la cuenta
+            private String _contenedor;
+
+            public AzureStorageUtils(String cuenta, String clave, String contenedor)
+            {
+                StorageCredentials cred = new StorageCredentials(cuenta, clave); //paso las credenciales creando un objeto de tipo storagecredentials 
+                _cuenta = new CloudStorageAccount(cred, true);
+                _contenedor = contenedor;
+            }
+
+            private void ComprobarContenedor(String contenedor = null)
+            {
+                if (contenedor != null)
+                {
+                    _contenedor = contenedor;
+                }
+
+                var cliente = _cuenta.CreateCloudBlobClient();
+                var cont = cliente.GetContainerReference(_contenedor);
+                cont.CreateIfNotExists();
+
+            }
+
+            public void SubirFichero(Stream fichero, String nombre, String contenedor = null)
+            {
+                ComprobarContenedor(contenedor);
+
+                var client = _cuenta.CreateCloudBlobClient();
+                var cont = client.GetContainerReference(_contenedor);
+                var blob = cont.GetBlockBlobReference(nombre);
+
+                blob.UploadFromStream(fichero);
+                fichero.Close();
+            }
+
+        public void SubirFichero(byte[] fichero, String nombre, String contenedor = null)
+        {
+            ComprobarContenedor(contenedor);
+
+            var client = _cuenta.CreateCloudBlobClient();
+            var cont = client.GetContainerReference(_contenedor);
+            var blob = cont.GetBlockBlobReference(nombre);
+
+            blob.UploadFromByteArray(fichero,0,fichero.Length);
+            //aqui no se hace el cierrre del fichero porque no se puede cerrar un array de bytes
+        }
+
+        public byte[] RecuperarFichero(String nombre, String contenedor)
+            {
+                ComprobarContenedor(contenedor);
+
+                var client = _cuenta.CreateCloudBlobClient();
+                var cont = client.GetContainerReference(_contenedor);
+                var blob = cont.GetBlockBlobReference(nombre);
+
+                blob.FetchAttributes();
+                var lon = blob.Properties.Length;
+                var dest = new byte[lon];
+                blob.DownloadToByteArray(dest, 0);
+                return dest;
+            }
+    }
+}
